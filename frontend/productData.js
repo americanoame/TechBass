80563241f7c4f940ca16473b5d0716493f048399
Americano.Dev <andrenations@gmail.com>
2024-03-07 20:55:28 -0500

    added productData

---
+++ frontend/productData.js
@@ -0,0 +1,69 @@
+const data = [
+    {
+      _id: '1',
+      name: 'Home Security Camera  ',
+      image: '/images/p5.webp',
+      price: '29.99',
+    },
+    {
+      _id: '2',
+      name: 'Laptop 14" HD Intel Celeron',
+      image: '/images/p2.webp',
+      price: '199.99',
+    },
+    {
+      _id: '3',
+      name: 'Apple Watch SE [GPS 40mm]',
+      image: '/images/p3.webp',
+      price: '29.99',
+    },
+    {
+      _id: '4',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p4.webp',
+      price: '29.99',
+    },
+    {
+      _id: '5',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p5.webp',
+      price: '29.99',
+    },
+    {
+      _id: '6',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p7.webp',
+      price: '29.99',
+    },
+    {
+      _id: '7',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p7.webp',
+      price: '29.99',
+    },
+    {
+      _id: '8',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p6.webp',
+      price: '29.99',
+    },
+    
+    {
+      _id: '9',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p3.webp',
+      price: '29.99',
+    },
+    {
+      _id: '10',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/p5.webp',
+      price: '29.99',
+    },
+    
+    
+    
+  ];
+  
+  export default data;
+  
