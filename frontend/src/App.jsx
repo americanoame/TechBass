8feaac457159632299b97d995460edbb263faff6
Americano.Dev <andrenations@gmail.com>
2024-03-07 20:05:53 -0500

    added home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/navbar/Nav';
+import Home from './pages/home/Home';
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
