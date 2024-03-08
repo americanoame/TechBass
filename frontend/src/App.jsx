9a37b73301c9b1bb48947128ddeed098b97b445e
Americano.Dev <andrenations@gmail.com>
2024-03-07 20:59:40 -0500

    added product Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/navbar/Nav';
 import Home from './pages/home/Home';
+import Product from './pages/product/Product';
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
