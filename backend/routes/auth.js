95a58ad66055472a347d0cdc1b802122cbd42486
Americano.Dev <andrenations@gmail.com>
2024-03-16 22:31:54 -0400

    added auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,14 @@
+// auth.js
+
+import express from "express";
+import { loginController, registerController } from '../controllers/authController.js'
+
+// router object
+const router = express.Router()
+
+router.post('/register', registerController)
+router.post('/login', loginController)
+
+
+
+export default router;
