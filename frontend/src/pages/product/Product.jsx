1b9aedf5af2f27664362fd7955b9e0fed2b10a9d
Americano.Dev <andrenations@gmail.com>
2024-03-17 02:24:13 -0400

    this function will not allow the user to see the project page when they are not login

--- frontend/src/pages/product/Product.jsx
+++ frontend/src/pages/product/Product.jsx
@@ -1,12 +1,33 @@
 import Card from '../../pages/card/Card';
-import productData from '../../../productData'; 
+import productData from '../../../productData';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+// this code will not allow the user to see the product page
+// they are not login
 
 const Product = () => {
+  const navigate = useNavigate();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('data');
+    if (!storedData) {
+      setIsLoggedIn(false);
+      navigate('/'); // Redirect to home if not logged in
+    }
+  }, [navigate]);
+
+  if (!isLoggedIn) {
+    return null; // Return null or any other component if not logged in
+  }
+
   return (
     <div>
       <div className="product">
         <div className="layout">
-            <Card products={productData} /> 
+          <Card products={productData} />
         </div>
       </div>
     </div>
