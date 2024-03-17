bb2fe0e8279ad977a4dd0cb36f8d770c4a06bf60
Americano.Dev <andrenations@gmail.com>
2024-03-16 22:30:38 -0400

    added useModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+// userModel.js
+
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+export default mongoose.model('users', userSchema);
