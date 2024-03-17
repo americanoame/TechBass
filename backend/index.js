54791a8937a2963fa04bc98cd64394a26fbd6fee
Americano.Dev <andrenations@gmail.com>
2024-03-16 22:21:23 -0400

    added cors, authRoutes and consoling for debugging

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,25 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connnectDb from './config/db.js';
+import authRoutes from './routes/auth.js';
+import cors from 'cors';
 
 const port = process.env.PORT || 8000;
-
-
 dotenv.config();
 const app = express();
-
 connnectDb();
-
 app.use(express.json());
+app.use(cors());
+
+// Consoling requset.body for debugging
+app.use((req, res, next) => {
+  console.log(req.body);
+  next();
+});
+
+
+app.use('/api/auth', authRoutes);
+
 
 app.get('/', (req, res) => {
   return res.send({
